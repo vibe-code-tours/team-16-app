af335c9df06466d5418e8e96b7052039c57c4a5a
Myo Min Lin <myominlin92@gmail.com>
2026-07-13 15:13:39 +0900

    Fix failed backend api tests which is blocking the Github CI

--- src/api/src/test/java/com/nerdquiz/controller/QuestionControllerTest.java
+++ src/api/src/test/java/com/nerdquiz/controller/QuestionControllerTest.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(QuestionController.class)
+@AutoConfigureMockMvc(addFilters = false)
 @TestPropertySource(properties = {"supabase.url=https://test.supabase.co"})
 class QuestionControllerTest {
 
